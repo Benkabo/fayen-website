98c95a2d17b2cafde66fd38d14a1e1559fad1b9d
Benkabo <kaboyokabenedict@gmail.com>
2024-10-03 09:02:10 +0300

    Fix about page image

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 
-import treeImage from "@/public/images/tree.png";
+import treeImage from "@/public/images/tree-1.png";
 
 export default function About() {
   return (
-    <div className="max-w-screen-lg min-h-[500px] mx-auto ">
+    <div className="max-w-screen-lg min-h-[500px] m-auto">
       <div className="flex items-center justify-center gap-4">
         <div className="hidden lg:flex gap-2">
-          <Image src={treeImage} alt="tree images" placeholder="blur" />
+          <Image
+            src={treeImage}
+            width={500}
+            alt="tree images"
+            className="rounded-md"
+          />
         </div>
         <div className="sm:w-[500px] text-center sm:text-start sm:m-0">
           <div className="text-4xl text-center md:text-left font-bold text-[#01306A] mb-4">
