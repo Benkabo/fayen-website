11d3106b50707254fc8765e38ce915740510bdfe
Benkabo <kaboyokabenedict@gmail.com>
2024-10-10 03:58:35 +0300

    Delete unused value

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -6,22 +6,16 @@ export default function Testimonials() {
       id: 1,
       text: "Fayen Travel Agency made booking my flights so easy! I was struggling to find the right connection for my business trip, but their team found the perfect option, saving me both time and money. The whole process was smooth, and I received all my confirmations right away. I’ll definitely use their service again for future travels!",
       author: "Benedict",
-      photo:
-        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
     },
     {
       id: 2,
       text: "I rented a car through Fayen Travel Agency for a family road trip, and I couldn’t be happier with the service. The car was in great condition, and they provided plenty of options to suit our needs. The booking process was simple, and their customer support was top-notch. We had a hassle-free experience from start to finish!",
       author: "Khalid Aucho",
-      photo:
-        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     },
     {
       id: 3,
       text: "I was nervous about the visa process for my upcoming trip, but Fayen Travel Agency took care of everything. They guided me through each step, ensuring all my paperwork was in order. Thanks to their expertise, I received my visa on time without any issues. I highly recommend their visa registration service!",
       author: "Declan Rice",
-      photo:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     },
   ];
   return (
