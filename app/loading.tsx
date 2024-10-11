796a2d2a52ae32d3703b452cedc013d574ac7c02
Benkabo <kaboyokabenedict@gmail.com>
2024-10-11 18:25:02 +0300

    Add loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
+import { DotLoader } from "react-spinners";
+
 export default function Loading() {
   return (
-    <>
-      <div className="flex items-center justify-center">Loading.....</div>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <DotLoader color="#013f86" loading size={80} />
+    </div>
   );
 }
