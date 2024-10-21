72cdb37492cc32568fc354b9d523f1fb389fd535
Benkabo <kaboyokabenedict@gmail.com>
2024-10-21 15:53:56 +0300

    Fix image size on safari

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,7 @@ import { FaArrowDownLong } from "react-icons/fa6";
 export default function AboutPage() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img-atcl">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/10 z-[2]" />
+      <div className="flex items-center justify-center h-screen custom-img-atcl">
         <div>
           <div className="text-white">
             <p className="text-4xl md:text-7xl lg:text-9xl font-bold text-center tracking-widest">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,9 @@ export default function Hero() {
   return (
     <div
       id="/"
-      className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img"
+      className="flex items-center justify-center h-screen custom-img"
     >
       {/* overlay */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/25 z-[2]" />
       <div className="text-white z-[2]">
         <h2 className="text-4xl md:text-7xl lg:text-9xl">Discover your next</h2>
         <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold text-center tracking-widest">
